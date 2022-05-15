2acfeb60b834dbf697550746c5805b68e033550f
Josh <jreginaldo@protonmail.com>
2022-05-15 18:51:51 -0500

    Revert "Display coin name in embed"
    This reverts commit 32b9d09ff82f12c0b425a0e6442bf97fd9ddce55.

--- src/bot.py
+++ src/bot.py
@@ -1,4 +1,3 @@
-import hikari
 import lightbulb
 import os
 from dotenvy import load_env, read_file
@@ -13,8 +12,7 @@ DEV_SERVER_ID = os.environ.get("DEV_SERVER_ID")
 
 # ---------- BOT INITIALIZATION ---------- #
 bot = lightbulb.BotApp(
-    token=BOT_SECRET,
-    intents=hikari.Intents.ALL
+    token=BOT_SECRET
 )
 tasks.load(bot)
 
@@ -35,29 +33,13 @@ async def ping(ctx):
     await ctx.respond("response!")
 
 
-@bot.command
-@lightbulb.command("activity", "get user activity!")
-@lightbulb.implements(lightbulb.SlashCommand)
-async def act(ctx):
-    # bot_presence = ctx.get_guild().get_my_member().get_presence()  # Bot
-    user_presence = ctx.member.get_presence()  # User who called command
-
-    # print(bot_presence.activities)  # Bot
-    print(user_presence.activities)  # User who called command - will throw error if user is invisible
-    user_current_game = user_presence.activities[1].name
-    if user_current_game.find("PyCharm") != -1:
-        print(f"User is currently playing {user_current_game}")
-
 # ----------     MAIN LINE      ---------- #
 if __name__ == '__main__':
 
     # ----------    BACKGROUND TASKS    ---------- #
-    # Update coin database to reflect 5-min changes
-    @tasks.task(s=300)
+    @tasks.task(s=300, auto_start=True)  # Run every 5 min
     async def refresh_coins():
         update_coins()
         print("Coins successfully updated")
 
-    # ----------    RUN BOT & TASKS    ---------- #
-    # refresh_coins.start()
     bot.run()
